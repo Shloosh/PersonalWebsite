ca355cdd499907885708c30c5d62eed0fc580b1d
Brian <bschmoker733@gmail.com>
2016-09-06 22:02:25 -0600

    Added basic html and css files. Updated file server.

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-// Load the http module to create an http server.
-var http = require('http');
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs")
+    port = process.argv[2] || 8000;
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Wordl\n");
-});
+http.createServer(function(request, response) {
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+  var uri = url.parse(request.url).pathname
+    , filename = path.join(process.cwd(), uri);
+  
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
 
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {        
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
