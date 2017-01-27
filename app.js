7ced71a31c7a834161a1ec7ac7a3349cd28249fb
Brian <bschmoker733@gmail.com>
2017-01-26 21:43:41 -0700

    Added middleware to catch and 404 any get requests containing .html or .htm

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-var express = require('express')
+var express = require('express'),
+    url = require('url')
 var app = express()
 
-app.use(express.static('src/', {
-    extensions: ['html', 'htm']
+app.use(function (req, res, next) {
+  var uri = url.parse(req.url).pathname
+    , file = uri.substring(uri.lastIndexOf('/') + 1);
+
+  if (file.contains('.html') || file.contains('.htm')) {
+    res.status(404).send('Cannot GET ' + uri);
+  }
+
+  next()
+})
+
+app.use('/', express.static('src/', {
+  index: 'home.html',
+  extensions: ['html', 'htm']
 }))
 
+/*
 app.get('/', function(req, res) {
-  res.send("Hello World!")
+  res.sendfile('home.html', { root: __dirname + '/src/' })
 })
+*/
 
 app.post('/', function (req, res) {
   res.send('Got a POST request')
 })
 
 app.listen(8000, function() {
-  console.log("hello world express")
+  console.log("Starting server...")
 })
+
+// Helpers
+
+Object.prototype.contains = function(containedStr) {
+  return (this.indexOf(containedStr) != -1)
+}
