c8b01efc020dff1833a3e40c7786c029cc62c36c
Brian <bschmoker733@gmail.com>
2018-11-16 00:37:31 -0700

    Fixed Push_to_Production script

--- Push_to_Production.sh
+++ Push_to_Production.sh
@@ -5,9 +5,11 @@ serverHost="138.68.241.190"
 serverPEM="/home/shloosh/Desktop/Programming/Server Connection Files/DigitalOcean.pem"
 localDir="/home/shloosh/Desktop/PersonalWebsite"
 serverDir="/var/www/brianschmoker.com"
+# For whatever reason, some profile info isn't loading for remote ssh and pm2 is not in the path variable
+pm2="/home/ubuntu/.nvm/versions/node/v8.9.3/bin/pm2"
 
 echo "Stopping app.js in pm2"
-ssh -i "$serverPEM" $serverUser@$serverHost "pm2 stop app"
+ssh -i "$serverPEM" $serverUser@$serverHost "$pm2 stop brianschmoker.com"
 echo "Removing website files on remote server"
 ssh -i "$serverPEM" $serverUser@$serverHost "rm -rf $serverDir/*"
 echo "Backing up website"
@@ -21,4 +23,4 @@ ssh -i "$serverPEM" $serverUser@$serverHost "rm $serverDir/website_backup.tar.gz
 echo "Adding execute permissions to app.js"
 ssh -i "$serverPEM" $serverUser@$serverHost "chmod u+x $serverDir/app.js"
 echo "Starting app.js with pm2"
-ssh -i "$serverPEM" $serverUser@$serverHost "pm2 start $serverDir/app.js"
+ssh -i "$serverPEM" $serverUser@$serverHost "$pm2 start $serverDir/app.js --cwd='$serverDir/' --name='brianschmoker.com'" 
